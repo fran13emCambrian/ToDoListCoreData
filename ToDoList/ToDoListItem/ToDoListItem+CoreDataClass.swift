5c15e49b26e2b127940d5817364d40710ec9520c
Francisco Escobar <91104756+fran13emCambrian@users.noreply.github.com>
2022-03-17 18:43:01 -0230

    #ToDoApp

---
+++ ToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoList
+//
+//  Created by Francisco Escobar on 2022-03-17.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
